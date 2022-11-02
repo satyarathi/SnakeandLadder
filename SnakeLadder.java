9d149647288c274d513e4504f0d59b522c18c934
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-02 14:48:00 +0530

    first commit

---
+++ SnakeLadder.java
@@ -0,0 +1,15 @@
+package com.day.four;
+
+public class SnakeLadder {
+	int startposition = 0;
+	int endposition = 100;
+	int currentposition = 0; 
+	int oldposition = 0;
+	int newposition = 0;
+
+	public static void main(String[] args) {
+		SnakeLadder sp = new SnakeLadder();
+		System.out.println(sp.startposition);
+
+	}
+}
