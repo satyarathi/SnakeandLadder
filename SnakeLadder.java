b204db4dfeb2394c01c2744547de2c997be5c0b3
satyarathi <jitendrasatyarathi@gmail.com>
2022-11-02 23:14:09 +0530

    third commit

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,17 +8,56 @@ public class SnakeLadder {
 	int oldposition = 0;
 	int newposition = 0;
 	int diceroll = 0;
+	
+	//option
+	public  int option(){
+		int Noplay = 0;
+		int Ladder = 1;
+		int Snake = 2;
+		Random opt = new Random();
+		int n = opt.nextInt(3);
+		if (n == Noplay) {
+			System.out.println("noplay");
+			return n;
+		}
+		if (n == Ladder) {
+			System.out.println("ladder");
+			return n;
+		}
+		if (n == Snake) {
+			System.out.println("snake");
+			return n;
+		}
+		return n;
+	
+		}
+
+ 
 
 	public static void main(String[] args) {
 		SnakeLadder sp = new SnakeLadder();
-		System.out.println(sp.startposition);
+		System.out.println("Initialposition :" +sp.startposition);
 
 		//dice roll
 		Random rolladice = new Random();
 		int diceroll = rolladice.nextInt(7);
 		if (diceroll == 0) {
 			diceroll++;
-		}
-		System.out.println(diceroll);
+		}	
+		System.out.println("Dice rolled :" +diceroll);
+		
+		// option
+		SnakeLadder j = new SnakeLadder();
+		j.option();
+		
+		
+		
 	}
-}
+	
+	
+		
+		
+	}
+	
+
+
